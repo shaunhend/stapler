e731d416a62589b7181b9cb65d2455f8252758c1
shaunhend <shaun.henderson@fifthring.com>
2014-08-16 14:12:55 +0100

    Update Validator.php

--- src/Codesleeve/Stapler/Validator.php
+++ src/Codesleeve/Stapler/Validator.php
@@ -22,7 +22,7 @@ class Validator
 	 */
 	protected function validateFilesystemOptions(array $options)
 	{
-		if (preg_match("/:id\b/", $options['url']) !== 1 && preg_match("/:id_partition\b/", $options['url']) !== 1 && preg_match("/:hash\b/", $options['url']) !== 1) {
+		if (preg_match("/:id\b/", $options['url']) !== 1 && preg_match("/\b/", $options['url']) !== 1 && preg_match("/:hash\b/", $options['url']) !== 1) {
 			throw new Exceptions\InvalidUrlOptionException('Invalid Url: an id, id_partition, or hash interpolation is required.', 1);
 		}
 	}
